faccd033d40a4451e0875f41a9854b4c1fb06eca
Joel Wigginton <joel@pragmatikprogramming.com>
2014-03-29 17:55:40 -0700

    1.7.17
    more updates

--- CMS.WebUI/Controllers/HomeController.cs
+++ CMS.WebUI/Controllers/HomeController.cs
@@ -207,7 +207,7 @@ namespace CMS.WebUI.Controllers
                 ViewBag.PageType = m_Page.PageType;
                 ViewBag.id = m_Page.PageTypeId;
                 ViewBag.isPostBack = 1;
-                ViewBag.SuccessPage = m_Form.Success;
+                ViewBag.Success = m_Form.Success;
                 ViewBag.PageId = m_Page.TemplateId;
                 ViewBag.TemplateId = m_Page.TemplateId;
                 //ViewBag.Message = "Your information has been submitted.";
